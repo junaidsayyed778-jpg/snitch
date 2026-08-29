2cc116470d95e73a3dbac5162030bc5e3509db6f
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-08-29 19:51:30 +0530

    refactor: centralize JWT token generation

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,74 +1,54 @@
 import userModel from "../models/userModel.js";
-import jwt from "jsonwebtoken";
-import { config } from "../config/config.js";
+import { generateToken } from "../utils/token.js";
 
 export async function loginUser(email, password) {
-    const user = await userModel.findOne({ email });
+  const user = await userModel.findOne({ email });
 
-    if (!user) {
-        throw new Error("Invalid email or password");
-    }
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
 
-    const isMatch = await user.comparePassword(password);
+  const isMatch = await user.comparePassword(password);
 
-    if (!isMatch) {
-        throw new Error("Invalid email or password");
-    }
+  if (!isMatch) {
+    throw new Error("Invalid email or password");
+  }
 
-    const token = jwt.sign(
-        {
-            id: user._id,
-        },
-        config.JWT_SECRET,
-        {
-            expiresIn: "7d",
-        }
-    );
+  const token = generateToken(user._id);
 
-    return {
-        token,
-        user,
-    };
+  return {
+    token,
+    user,
+  };
 }
 
 export async function registerUser({
+  email,
+  contact,
+  password,
+  fullname,
+  isSeller,
+}) {
+  const existingUser = await userModel.findOne({
+    $or: [{ email }, { contact }],
+  });
+
+  if (existingUser) {
+    throw new Error("User already exists");
+  }
+
+  const user = await userModel.create({
     email,
     contact,
     password,
     fullname,
-    isSeller
-}) {
-    const existingUser = await userModel.findOne({
-        $or: [
-            { email },
-            { contact }
-        ]
-    });
+    role: isSeller ? "seller" : "buyer",
+  });
 
-    if (existingUser) {
-        throw new Error("User already exists");
-    }
+  const token = generateToken(user._id);
 
-    const user = await userModel.create({
-        email,
-        contact,
-        password,
-        fullname,
-        role: isSeller ? "seller" : "buyer"
-    });
-
-    const token = jwt.sign(
-        {
-            id: user._id,
-        },
-        config.JWT_SECRET,
-        {
-            expiresIn: "7d",
-        }
-    );
-
-    return {
-        token,
-        user,
-    };
+  return {
+    token,
+    user,
+  };
 }

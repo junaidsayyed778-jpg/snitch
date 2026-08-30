b9ad30fe3c6e2702dc7bed8050c6e8e340ac682b
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-08-30 16:22:07 +0530

    refactor: centralize auth cookie handling

---
+++ backend/src/utils/cookie.js
@@ -0,0 +1,8 @@
+export function setAuthCookie(res, token) {
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    })
+}
